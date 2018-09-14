5f6343171c54ffe940928b74e6f1a3c2db841caf
Emilio Dalla Torre <emiliodallatorre12@live.com>
2018-09-14 16:43:36 +0200

    Created several fragments.
    When the fragments system will work, starting to work on features.

--- iterator/src/main/java/gq/emiliodallatorre/oneaday/iterator/MainActivity.kt
+++ iterator/src/main/java/gq/emiliodallatorre/oneaday/iterator/MainActivity.kt
@@ -1,29 +1,34 @@
 package gq.emiliodallatorre.oneaday.iterator
 
+import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainFragment.OnFragmentInteractionListener, DashboardFragment.OnFragmentInteractionListener {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
         when (item.itemId) {
             R.id.navigation_home -> {
                 findViewById<TextView>(R.id.message).text = getString(R.string.title_home)
-                val fragmentManager = supportFragmentManager
-                val fragmentTransaction = fragmentManager.beginTransaction()
-                val fragment = MainFragment()
-                fragmentTransaction.add(R.id.fragment_main, fragment)
+                fragmentTransaction.add(R.id.fragment_main, MainFragment())
                 fragmentTransaction.commit()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                findViewById<TextView>(R.id.message).text = getString(R.string.title_dashboard)
+                fragmentTransaction.replace(R.id.fragment_main, DashboardFragment())
+                fragmentTransaction.addToBackStack(null)
+                fragmentTransaction.commit()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
                 findViewById<TextView>(R.id.message).text = getString(R.string.title_notifications)
+                val fragment = MainFragment()
+                fragmentTransaction.add(R.id.fragment_main, fragment)
+                fragmentTransaction.commit()
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -34,5 +39,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         findViewById<BottomNavigationView>(R.id.navigation).setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    override fun onFragmentInteraction(uri: Uri) {
+        // TODO: To be understood.
     }
 }
