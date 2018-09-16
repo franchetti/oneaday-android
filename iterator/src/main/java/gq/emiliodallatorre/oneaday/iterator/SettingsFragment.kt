e51c0783a19efdd25ac02f8206cd86ca2aeced5a
Emilio Dalla Torre <emiliodallatorre12@live.com>
2018-09-16 14:36:41 +0200

    Made PreferenceFragment.kt work.

--- iterator/src/main/java/gq/emiliodallatorre/oneaday/iterator/SettingsFragment.kt
+++ iterator/src/main/java/gq/emiliodallatorre/oneaday/iterator/SettingsFragment.kt
@@ -3,8 +3,8 @@ package gq.emiliodallatorre.oneaday.iterator
 import android.os.Bundle
 import android.support.v7.preference.PreferenceFragmentCompat
 
-class SettingsFragment: PreferenceFragmentCompat() {
-    override fun onCreatePreferences(savedInstanceState: Bundle, rootKey: String) {
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         // Load the preferences from an XML resource
         setPreferencesFromResource(R.xml.settings, rootKey)
     }
