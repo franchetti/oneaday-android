b5668cbc756d49daac73560191c013e9bad3d6ea
Emilio Dalla Torre <emiliodallatorre12@live.com>
2018-09-16 20:57:20 +0200

    Updated settings to add button state change for enabling.

--- iterator/src/main/java/gq/emiliodallatorre/oneaday/iterator/SettingsFragment.kt
+++ iterator/src/main/java/gq/emiliodallatorre/oneaday/iterator/SettingsFragment.kt
@@ -1,11 +1,21 @@
 package gq.emiliodallatorre.oneaday.iterator
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.support.v7.preference.PreferenceFragmentCompat
+import android.support.v7.preference.PreferenceManager
 
-class SettingsFragment : PreferenceFragmentCompat() {
+class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
+    
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        // Load the preferences from an XML resource
+        // Load the preferences from an XML resource.
         setPreferencesFromResource(R.xml.settings, rootKey)
+        PreferenceManager.getDefaultSharedPreferences(context).registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        findPreference("apticFeedback").isEnabled = PreferenceManager.getDefaultSharedPreferences(context).getBoolean("notificationsSwitch", true)
+        findPreference("notificationsTime").isEnabled = PreferenceManager.getDefaultSharedPreferences(context).getBoolean("notificationsSwitch", true)
+        PreferenceManager.getDefaultSharedPreferences(context).registerOnSharedPreferenceChangeListener(this)
     }
 }
