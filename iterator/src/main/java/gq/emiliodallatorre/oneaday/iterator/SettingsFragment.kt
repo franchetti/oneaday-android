1d6c5c8629585e1fba45cf7a3ac05e4967c12d2a
Emilio Dalla Torre <emiliodallatorre12@live.com>
2018-09-17 16:09:45 +0200

    Setted theme to use Roboto font.

--- iterator/src/main/java/gq/emiliodallatorre/oneaday/iterator/SettingsFragment.kt
+++ iterator/src/main/java/gq/emiliodallatorre/oneaday/iterator/SettingsFragment.kt
@@ -5,8 +5,8 @@ import android.os.Bundle
 import android.support.v7.preference.PreferenceFragmentCompat
 import android.support.v7.preference.PreferenceManager
 
-class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
-    
+class SettingsFragment: PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         // Load the preferences from an XML resource.
         setPreferencesFromResource(R.xml.settings, rootKey)
