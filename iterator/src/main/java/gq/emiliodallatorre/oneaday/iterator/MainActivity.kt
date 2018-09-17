ebbdf6b707583be240cc5e1b819f457f551bde32
Emilio Dalla Torre <emiliodallatorre12@live.com>
2018-09-17 17:09:27 +0200

    Setted Comfortaa font for Dashboard title.

--- iterator/src/main/java/gq/emiliodallatorre/oneaday/iterator/MainActivity.kt
+++ iterator/src/main/java/gq/emiliodallatorre/oneaday/iterator/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(), MainFragment.OnFragmentInteractionListener, DashboardFragment.OnFragmentInteractionListener {
+
+
+class MainActivity: AppCompatActivity(), MainFragment.OnFragmentInteractionListener, DashboardFragment.OnFragmentInteractionListener {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         val fragmentManager = supportFragmentManager
