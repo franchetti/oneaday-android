9cbd3a79cbf5c6f5860580fb6b9a28b8250322fa
Emilio Dalla Torre <emiliodallatorre12@live.com>
2018-10-20 18:57:54 +0200

    Setted up AdviceView class.

--- iterator/src/main/java/gq/emiliodallatorre/oneaday/iterator/AdviceView.kt
+++ iterator/src/main/java/gq/emiliodallatorre/oneaday/iterator/AdviceView.kt
@@ -1,10 +1,8 @@
 package gq.emiliodallatorre.oneaday.iterator
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
-
-import kotlinx.android.synthetic.main.advice_view.*
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 
 class AdviceView : AppCompatActivity() {
 
@@ -12,6 +10,9 @@ class AdviceView : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.advice_view)
         // TODO: Implement a view for the advice, and add according links.
+        print(intent.getStringExtra("dayOfPath"))
+        findViewById<TextView>(R.id.advice_view_title).text = intent.getStringExtra("advice")
+        findViewById<TextView>(R.id.advice_view_body).text = resources.getStringArray(R.array.advicesBody)[Integer.parseInt(intent.getStringExtra("dayOfPath"))]
+        findViewById<TextView>(R.id.advice_view_body).text = resources.getStringArray(R.array.advicesBody)[0]
     }
-
 }
