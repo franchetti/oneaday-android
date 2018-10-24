23ac2aff17b628c8a5d35a58551483694a3d654a
Emilio Dalla Torre <emiliodallatorre12@live.com>
2018-10-24 19:39:12 +0200

    Removed unnecessary code.

--- iterator/src/main/java/gq/emiliodallatorre/oneaday/iterator/DashboardFragment.kt
+++ iterator/src/main/java/gq/emiliodallatorre/oneaday/iterator/DashboardFragment.kt
@@ -11,21 +11,6 @@ import android.view.ViewGroup
 import android.widget.ListView
 import java.util.*
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [MainFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [MainFragment.newInstance] factory method to
- * create an instance of this fragment.
- *
- */
 class DashboardFragment : Fragment() {
     private var listener: OnFragmentInteractionListener? = null
     private var adviceList = ArrayList<AdviceModel>()
@@ -106,25 +91,5 @@ class DashboardFragment : Fragment() {
     interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         fun onFragmentInteraction(uri: Uri)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                MainFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
     }
 }
