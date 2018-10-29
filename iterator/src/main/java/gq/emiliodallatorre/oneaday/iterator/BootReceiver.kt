2c68473c4e52991b75f8dd509dfec81999dce50d
Emilio Dalla Torre <emiliodallatorre12@live.com>
2018-10-29 11:03:43 +0100

    Temporary commit.

--- iterator/src/main/java/gq/emiliodallatorre/oneaday/iterator/BootReceiver.kt
+++ iterator/src/main/java/gq/emiliodallatorre/oneaday/iterator/BootReceiver.kt
@@ -6,27 +6,35 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.support.v7.preference.PreferenceManager
+import android.widget.Toast
 import java.util.*
 
 class BootReceiver: BroadcastReceiver() {
-
     // TODO: Fix security issue.
     override fun onReceive(context: Context, intent: Intent) {
-        // This method is called when the BroadcastReceiver is receiving a BOOT_COMPLETED broadcast.
-        val pendingIntent = PendingIntent.getBroadcast(context, 1201, Intent(context, NotificationReceiver::class.java), PendingIntent.FLAG_UPDATE_CURRENT)
-        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        if (intent.action == "android.intent.action.BOOT_COMPLETED") {
+            // This method is called when the BroadcastReceiver is receiving a BOOT_COMPLETED broadcast.
+            if(!PreferenceManager.getDefaultSharedPreferences(context).getBoolean("persistentNotification", false)) {
+                val pendingIntent = PendingIntent.getBroadcast(context, 1201, Intent(context, NotificationReceiver::class.java), PendingIntent.FLAG_UPDATE_CURRENT)
+                val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
-        val calendar = Calendar.getInstance()
-        calendar.timeInMillis = System.currentTimeMillis()
+                val calendar = Calendar.getInstance()
+                calendar.timeInMillis = System.currentTimeMillis()
 
-        when (PreferenceManager.getDefaultSharedPreferences(context).getInt("notificationsTime", 0)) {
-            0 -> calendar.set(Calendar.HOUR_OF_DAY, 9)
-            1 -> calendar.set(Calendar.HOUR_OF_DAY, 12)
-            2 -> calendar.set(Calendar.HOUR_OF_DAY, 18)
+                when (PreferenceManager.getDefaultSharedPreferences(context).getInt("notificationsTime", 0)) {
+                    0 -> calendar.set(Calendar.HOUR_OF_DAY, 9)
+                    1 -> calendar.set(Calendar.HOUR_OF_DAY, 15)
+                    2 -> calendar.set(Calendar.HOUR_OF_DAY, 18)
+                }
+                calendar.set(Calendar.MINUTE, 37)
+                calendar.set(Calendar.SECOND, 0)
+
+                alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, (1000 * 60 * 60 * 24).toLong(), pendingIntent)
+            } else {
+                context.sendBroadcast(Intent(context, NotificationReceiver::class.java))
+            }
+            System.out.println("BOOOOOOOOOOOOOOOOOOOOTTTTTTTTTTTT")
+            Toast.makeText(context, "BOOOOOOOOOOOTTTTT", Toast.LENGTH_SHORT).show()
         }
-        calendar.set(Calendar.MINUTE, 0)
-        calendar.set(Calendar.SECOND, 0)
-
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, (1000 * 60 * 60 * 24).toLong(), pendingIntent)
     }
 }
