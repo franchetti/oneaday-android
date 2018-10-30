693d36c2c03ea67ea2919ef2cb9513abbed12c54
Emilio Dalla Torre <emiliodallatorre12@live.com>
2018-10-30 16:03:18 +0100

    Moved to newer progressBar with animations.

--- iterator/src/main/java/gq/emiliodallatorre/oneaday/iterator/MainFragment.kt
+++ iterator/src/main/java/gq/emiliodallatorre/oneaday/iterator/MainFragment.kt
@@ -3,13 +3,14 @@ package gq.emiliodallatorre.oneaday.iterator
 import android.app.Activity
 import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
 import android.support.v4.app.Fragment
 import android.support.v7.preference.PreferenceManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import com.github.lzyzsd.circleprogress.DonutProgress
+import com.bikomobile.donutprogress.DonutProgress
 import java.util.*
 
 
@@ -34,14 +35,25 @@ class MainFragment: Fragment() {
     override fun onStart() {
         super.onStart()
         // TODO: Setup things for lastDay of path.
-        val donutProgress: DonutProgress = (context as Activity).findViewById(R.id.progressBar) as DonutProgress
+        val donutProgress: DonutProgress = (context as Activity).findViewById(R.id.progressBar)
         val dayOfPath: Int = Calendar.getInstance().get(Calendar.DAY_OF_MONTH) - PreferenceManager.getDefaultSharedPreferences(context).getInt("startDay", 1)
-        ((context as Activity).findViewById(R.id.advice_title) as TextView).text = resources.getStringArray(R.array.advicesTitle)[dayOfPath]
-        ((context as Activity).findViewById(R.id.advice_subtitle) as TextView).text = resources.getStringArray(R.array.advicesSubtitle)[dayOfPath]
-        donutProgress.text = getString(R.string.main_day) + " " + (dayOfPath + 1) + " " + getString(R.string.main_of) + " 28"
-        donutProgress.progress = (dayOfPath + 1).toFloat()
-        donutProgress.finishedStrokeWidth = (100).toFloat()
-        donutProgress.unfinishedStrokeWidth = (20).toFloat()
+        ((context as Activity).findViewById<TextView>(R.id.advice_title)).text = resources.getStringArray(R.array.advicesTitle)[dayOfPath]
+        ((context as Activity).findViewById<TextView>(R.id.advice_subtitle)).text = resources.getStringArray(R.array.advicesSubtitle)[dayOfPath]
+        donutProgress.text = dayOfPath.toString()
+        donutProgress.max = 28 * 10
+        val progressText: String = getString(R.string.main_day) + " " + (dayOfPath + 1) + " " + getString(R.string.main_of) + " 28"
+        ((context as Activity).findViewById<TextView>(R.id.progressText)).text = progressText
+
+        object : Thread() {
+            override fun run() {
+                donutProgress.setProgressWithAnimation((dayOfPath * 10), 10)
+            }
+        }.start()
+        Handler().postDelayed({
+            // Here you can do something on the exact moment the animation finish.
+            // TODO: Do something.
+        }, (dayOfPath * 10 * 10).toLong())
+
     }
 
     /**
